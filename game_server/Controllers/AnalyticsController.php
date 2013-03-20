f72185afc6d85a57de8d2f8a82d7e14cfffdc1b5
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-20 14:25:41 -0400

    added amcharts

---
+++ game_server/Controllers/AnalyticsController.php
@@ -0,0 +1,11 @@
+<?php
+
+public class AnalyticsController{
+	function __construct(){
+		echo "working";
+	}
+
+	public function getView(){
+		return "Stats1.php";
+	}
+} // end AnalyticsController class def 
