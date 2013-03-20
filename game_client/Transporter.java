1c86a5628d89e9408504b7eeba4b941576c86246
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-20 10:59:13 -0400

    working on gson

--- game_client/Transporter.java
+++ game_client/Transporter.java
@@ -3,9 +3,11 @@ import java.net.*;
 import org.apache.commons.net.*;
 import org.apache.http.client.*;
 import org.apache.http.impl.client.*;
+import com.google.gson.*;
 
 public class Transporter{
-	private String urlString = "http://slimdowndesign.com/cs205-Final_Project/game_server/?gamedata=newdata"; 
+	// private String urlString = "http://slimdowndesign.com/cs205-Final_Project/game_server/?gamedata=newdata"; 
+	private String urlString = "http://localhost/205_final_server/?gamedata=newdata"; 
 
 	public Transporter(gameState currentState){
 
@@ -20,24 +22,24 @@ public class Transporter{
 		try{
 			String urlParameters = "param1=a&param2=b&param3=c";
 			URL url = new URL(this.urlString); 
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			URLConnection connection = url.openConnection();
 			connection.setDoOutput(true);
 			connection.setDoInput(true);
-			connection.setInstanceFollowRedirects(false); 
-			connection.setRequestMethod("POST"); 
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
-			connection.setRequestProperty("charset", "utf-8");
-			connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-			connection.setUseCaches (false);
-			DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
-			wr.writeBytes(urlParameters);
-			wr.flush();
-			wr.close();
-			connection.disconnect();
+			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+			writer.write(urlParameters);
+			writer.flush();
+			String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			// displaying console output
+			// while ((line = reader.readLine()) != null) {
+			// 	System.out.println(line);
+			// }
+			writer.close();
+			reader.close();
 			System.out.println("Post complete");
 		}catch (IOException ex){
 			// handl exception
-			System.out.println("Could not connect to server");
+			System.out.println("Could not connect to server:  "+ex);
 		}
 	}
 }
