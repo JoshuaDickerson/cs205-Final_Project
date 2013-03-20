8177fc3d2a61a6b9ff9d6cd04ef143fb9cbc5a6a
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-20 09:49:54 -0400

    Added a test for the transporter, and successfully posted to server

--- game_client/Transporter.java
+++ game_client/Transporter.java
@@ -5,21 +5,39 @@ import org.apache.http.client.*;
 import org.apache.http.impl.client.*;
 
 public class Transporter{
-	private String url = "http://slimdowndesign.com/cs205-Final_Project/game_server/?gamedata=newdata"; 
+	private String urlString = "http://slimdowndesign.com/cs205-Final_Project/game_server/?gamedata=newdata"; 
 
 	public Transporter(gameState currentState){
 
 	}
 
+	public Transporter(String testContent){
+		this.postToServer();
+	}
+
 
 	public void postToServer(){
-		HttpClient httpClient = new DefaultHttpClient();
-
-		try {
-			URL urlObj = new URL(this.url);
-		} catch (MalformedURLException ex) {
-			// handle exception
+		try{
+			String urlParameters = "param1=a&param2=b&param3=c";
+			URL url = new URL(this.urlString); 
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setInstanceFollowRedirects(false); 
+			connection.setRequestMethod("POST"); 
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
+			connection.setRequestProperty("charset", "utf-8");
+			connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+			connection.setUseCaches (false);
+			DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+			connection.disconnect();
+			System.out.println("Post complete");
+		}catch (IOException ex){
+			// handl exception
+			System.out.println("Could not connect to server");
 		}
-		HttpURLConnection urlConn = null;
 	}
 }
