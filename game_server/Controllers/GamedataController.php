c32644d5d0dd2783578b178a6a77336eed34c8da
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-24 17:07:01 -0400

    gamestate populating database tables nicely

--- game_server/Controllers/GamedataController.php
+++ game_server/Controllers/GamedataController.php
@@ -14,18 +14,63 @@ class GamedataController{
 	
 	function __construct($actions, $POST, $debug = false){
 		$this->jsonObj = json_decode($POST['gameState']);
+		$this->addGame();
 		$this->addState();
 	}
 
 
 	public function addGame(){
+		// check if we already have this game in the DB
+		$gameID = $this->jsonObj->uniqueID;
+		$array = array(
+			'tableName'=>'tblGame',
+			'pkGameID'=>$gameID
+		);
+		$dbWrapper = new InteractDB('select', $array);
+		// logThis($dbWrapper);
+		// if we don't have this game, add it
+		if(count($dbWrapper->returnedRows) < 1){
+		// 	// first get the player IDs
+			$p1Email = $this->jsonObj->allPlayers[0]->playername;
+			// logThis($p1Email);
+			$p2Email = $this->jsonObj->allPlayers[1]->playername;
+			$uid1 = $this->emailToUserID($p1Email);
+			// logThis($uid1);
+			$uid2 = $this->emailToUserID($p2Email);
+			$array2 = array(
+				'tableName'=>'tblGame',
+				'pkGameID'=>$gameID, 
+				'fkUserID_p1'=>$uid1,
+				'fkUserID_p2'=>$uid2,
+				'fldMode'=>$this->jsonObj->state->mode,
+				'fldWinCond'=>$this->jsonObj->state->winCon
+			);
 
+			$dbWrapper = new InteractDB('insert', $array2);
+			// logThis($dbWrapper);
+		}
 	}
 
 	public function addState(){
 		// logThis("inside add state");
 		$gs = new GameState($this->jsonObj); 
 	} // end addState
+
+	public function emailToUserID($userEmailString){
+		$array = array(
+			'tableName'=>'tblUserAccount',
+			'fldEmail'=>$userEmailString
+		);
+		$dbWrapper=new InteractDB('select', $array);
+		if(count($dbWrapper->returnedRows) > 0){
+			$userID = $dbWrapper->returnedRows[0]['pkUserID'];
+			// $userID = 888;
+		}else{
+			$userID = 666;
+		}
+
+		return $userID;
+	}
 
 	public function getVars(){
 		return false;
