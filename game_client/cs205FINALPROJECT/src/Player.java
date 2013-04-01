85555035f5f0aea58795d5eb2dec4c061e26d369
popwarfour <popwarfour@hotmail.com>
2013-04-01 11:24:29 -0400

    after git add, hope this works

---
+++ game_client/cs205FINALPROJECT/src/Player.java
@@ -0,0 +1,80 @@
+class Player
+{
+	private String playername;
+	private boolean isHuman;
+	private int id;
+	private int score;
+	private int roundsWon;
+	private int totalScore;
+	public Hand myHand;
+	
+	//constructor
+	public Player(boolean isHuman, int id, String name, Hand myHand)
+	{
+		this.isHuman = isHuman;
+		this.id = id;
+		this.playername = name;
+		this.myHand = myHand;
+		this.roundsWon = 0;
+		this.totalScore = 0;
+	}
+	
+	public void setHand(Hand myHand)
+	{
+		this.myHand = myHand;
+	}
+	
+	public Hand getHand()
+	{
+		return this.myHand;
+	}	
+	
+	public void setHuman(boolean isHuman)
+	{
+		this.isHuman = isHuman;
+	}
+	public void setScore(int score)
+	{
+		this.score = score;
+	}
+	public void setID(int id)
+	{
+		this.id = id;
+	}
+	public void setName(String name)
+	{
+		playername = name;
+	}
+	public void setRoundsWon(int _roundsWon)
+	{
+		this.roundsWon = _roundsWon;
+	}
+	public int getRoundsWon()
+	{
+		return this.roundsWon;
+	}
+	public void setTotalScore(int _score)
+	{
+		this.totalScore = _score;
+	}
+	public int getTotalScore()
+	{
+		return this.totalScore;
+	}
+	public int getScore()
+	{
+		return score;
+	}
+	public boolean getHumanity()
+	{
+		return isHuman;
+	}
+	public int getID()
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return playername;
+	}
+}
